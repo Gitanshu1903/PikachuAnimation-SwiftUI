f27a97a09ebf737e0a5e7f4c963b49ee8282bf9e
Gitanshu1903 <gpvaghasiya1903@gmail.com>
2022-06-25 11:46:58 +0530

    Initial Commit

---
+++ LoginPageAnimation/LoginPageAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageAnimationApp.swift
+//  LoginPageAnimation
+//
+//  Created by ERASOFT on 07/04/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
